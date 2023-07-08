65405a32299976918df74532dc2e1a072c629829
Daniela Xavier <danielamxsoares@gmail.com>
2023-07-08 14:43:37 -0300

    criar endpoint para listagem de médicos

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -3,13 +3,17 @@ package med.voll.api.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastrosMedicos;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.MedicoEntity;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -20,5 +24,9 @@ public class MedicoController {
     @Transactional
     public void cadastroMedicos(@RequestBody @Valid DadosCadastrosMedicos dadosMedicos) {
         medicoRepository.save(new MedicoEntity(dadosMedicos));
+    }
+    @GetMapping
+    public List<DadosListagemMedico> listarMedicos() {
+      return medicoRepository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 }
