0f93dac7e4cddc06c14ff47df3646b82ffb439e8
Daniela Xavier <danielamxsoares@gmail.com>
2023-07-08 15:12:42 -0300

    incluir paginacao na listagem de medicos

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -7,6 +7,9 @@ import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.MedicoEntity;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,7 +29,7 @@ public class MedicoController {
         medicoRepository.save(new MedicoEntity(dadosMedicos));
     }
     @GetMapping
-    public List<DadosListagemMedico> listarMedicos() {
-      return medicoRepository.findAll().stream().map(DadosListagemMedico::new).toList();
+    public Page<DadosListagemMedico> listarMedicos(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+      return medicoRepository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 }
