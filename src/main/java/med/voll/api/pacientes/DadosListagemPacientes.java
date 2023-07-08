9dc3ace1ce15a49da8e451b0df24adf82ee19d9c
Daniela Xavier <danielamxsoares@gmail.com>
2023-07-08 15:38:43 -0300

    perf: incluir o id no retorno da requisição GET para pacientes

--- src/main/java/med/voll/api/pacientes/DadosListagemPacientes.java
+++ src/main/java/med/voll/api/pacientes/DadosListagemPacientes.java
@@ -2,8 +2,8 @@ package med.voll.api.pacientes;
 
 import med.voll.api.medico.MedicoEntity;
 
-public record DadosListagemPacientes(String nome, String email, String telefone, String cpf) {
+public record DadosListagemPacientes(Long id, String nome, String email, String telefone, String cpf) {
     public DadosListagemPacientes(PacientesEntity pacientes) {
-        this(pacientes.getNome(), pacientes.getEmail(), pacientes.getTelefone(), pacientes.getCpf());
+        this(pacientes.getId(), pacientes.getNome(), pacientes.getEmail(), pacientes.getTelefone(), pacientes.getCpf());
     }
 }
