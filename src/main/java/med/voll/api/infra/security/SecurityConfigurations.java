80fc2cfba8b5a6b1b933ed7586df8e8ed6d1fc68
Daniela Xavier <danielamxsoares@gmail.com>
2023-07-11 11:50:12 -0300

    perf:configurando a ordem de execução dos filter e a autenticação do token

--- src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -1,7 +1,9 @@
 package med.voll.api.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,15 +12,26 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
+    @Autowired
+    private SecurityFilter securityFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().build();
+                //Configurando autorizações
+                .and().authorizeHttpRequests()
+                //Configuração para que a requisição de login sempre seja autorizada:
+                .requestMatchers(HttpMethod.POST, "/login").permitAll() //request publica
+                .anyRequest().authenticated() //qualquer outra request precisa ser autenticada
+                //Configurando a ordem de chamada do filtros
+                //Primeiro executa o filtro de autenticação(SecurityFilter), depois o filtro nativo do Spring (UsernamePasswordAuthenticationFilter.class) que verifica se o usuário está logado
+                .and().addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
