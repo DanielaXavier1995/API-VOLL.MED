60c64619c7dde39cea403e213a7f813dad4077e4
Daniela Xavier <danielamxsoares@gmail.com>
2023-07-12 14:56:20 -0300

    feat: montar a Query jpql do método medicoAleatorio()

--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -3,7 +3,24 @@ package med.voll.api.domain.medico;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 public interface MedicoRepository extends JpaRepository<MedicoEntity, Long> {
     Page<MedicoEntity> findAllByAtivoTrue(Pageable paginacao);
+    @Query("""
+            SELECT m FROM medicos m
+            WHERE
+            m.ativo = 1
+            AND
+            m.id not in
+               (SELECT c.medico_id FROM consultas c
+               WHERE
+               c.data = :data) 
+            AND
+            m.especialidade = :especialidade
+            ORDER BY rand()
+            LIMIT 1
+            """)
+    MedicoEntity medicoAleatório(Especialidade especialidade, LocalDateTime data);
 }
