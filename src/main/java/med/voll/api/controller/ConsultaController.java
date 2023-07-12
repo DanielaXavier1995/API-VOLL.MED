e84b24cab0ea28ec1a4634ac600b2a18e13d40ab
Daniela Xavier <danielamxsoares@gmail.com>
2023-07-12 14:52:12 -0300

    perf: ajustar o endpoint para chamar o service

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -2,8 +2,10 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import med.voll.api.domain.consultas.AgendaDeConsultasService;
 import med.voll.api.domain.consultas.DadosAgendamentoConsulta;
 import med.voll.api.domain.consultas.DadosDetalhamentoConsulta;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,9 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("consultas")
 public class ConsultaController {
+    @Autowired
+    private AgendaDeConsultasService agendaDeConsultasService;
     @PostMapping
     @Transactional
     public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados) {
+        agendaDeConsultasService.agendar(dados);
         return ResponseEntity.ok(new DadosDetalhamentoConsulta(null, null, null, null));
     }
 
